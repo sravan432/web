91fb6ef7bdb83969dce705c2e948251b7ded2b7d
Sravan Kumar Vambala <84947333+sravan432@users.noreply.github.com>
2021-11-17 21:24:35 +0530

    Add files via upload

---
+++ JAVASCRIPT/javajs.js
@@ -0,0 +1,33 @@
+function javaintro()
+{
+    $( "#javaintro" ).load( "/javaintro.html");
+
+}
+function oopsIntro()
+{
+    $( "#Oopsintro" ).load( "../../Docs/javaintro.html" );   
+}
+function stringIntro()
+{
+    $( "#StringIntro" ).load("../../Docs/javaintro.html" );   
+}
+function multithreadingIntro()
+{
+    $( "#MultithreadingIntro" ).load( "../../Docs/javaintro.html" );   
+ }
+
+function exceptionIntro(){
+
+    $( "#ExceptionIntro" ).load( "../../Docs/javaintro.html" );
+}
+
+function collectionsIntro(){
+    $( "#CollectionsIntro" ).load( "../../Docs/javaintro.html" );
+}
+
+function javaPrograms(){
+    $( "#JavaPrograms" ).load( "../../Docs/javaintro.html" );
+}
+
+
+
